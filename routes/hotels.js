46e69f64b34a55da965b5d4aa8102ad9201dd73d
Dmitriifd <idimka9@gmail.com>
2023-09-15 17:00:25 +0500

    upd hotels route

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,15 +1,22 @@
 import express from 'express';
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from '../controllers/hotel.js';
 const router = express.Router();
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 // CREATE
-router.post('/', createHotel);
+router.post('/', verifyAdmin, createHotel);
 
 // UPDATE
-router.put('/:id', updateHotel);
+router.put('/:id', verifyAdmin, updateHotel);
 
 // DELETE
-router.delete('/:id', deleteHotel);
+router.delete('/:id', verifyAdmin, deleteHotel);
 
 // GET
 router.get('/find/:id', getHotel);
